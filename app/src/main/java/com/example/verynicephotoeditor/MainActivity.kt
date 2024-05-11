f060a3c8319cffdef473a74d0a57629cc65cc017
deadnya <isaa.saida@gmail.com>
2024-05-11 22:32:35 +0700

    📉 added FXAA (lagaet pizdec)

--- app/src/main/java/com/example/verynicephotoeditor/MainActivity.kt
+++ app/src/main/java/com/example/verynicephotoeditor/MainActivity.kt
@@ -19,6 +19,7 @@ import android.view.MotionEvent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.verynicephotoeditor.databinding.ActivityMainBinding
+import java.util.Vector
 import kotlin.math.abs
 import kotlin.math.pow
 import kotlin.math.sqrt
@@ -34,6 +35,8 @@ class MainActivity : AppCompatActivity() {
 
     private val mainDotSize = 20
     private val anchorDotSize = 10
+
+    private val FXAA_ON = true
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -71,7 +74,7 @@ class MainActivity : AppCompatActivity() {
         binding.imageView.layoutParams = layoutParams
 
         val spaceLookaround = 50
-        val scale = 2
+        val scale = 8
         val width = size.x / scale
         val height = size.y / scale
 
@@ -129,7 +132,7 @@ class MainActivity : AppCompatActivity() {
                         mainDotsList[anchorNextDotFirstIndex].getNextDot()?.changeY(event.y / scale)
                     }
 
-                    mutableBitmap.eraseColor(Color.TRANSPARENT);
+                    mutableBitmap.eraseColor(Color.WHITE);
                     update(mainDotsList, mutableBitmap)
                 }
 
@@ -178,7 +181,7 @@ class MainActivity : AppCompatActivity() {
                         }
                     }
 
-                    mutableBitmap.eraseColor(Color.TRANSPARENT);
+                    mutableBitmap.eraseColor(Color.WHITE);
                     update(mainDotsList, mutableBitmap)
                     isMoving = false
                 }
@@ -289,8 +292,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
-        binding.imageView.setImageBitmap(mutableBitmap)
     }
 
     private fun dist(x1: Double, y1: Double, x2: Double, y2: Double) : Double {
@@ -323,6 +324,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         drawSpline(dotsList, mutableBitmap)
+
+        binding.imageView.setImageBitmap(mutableBitmap)
     }
 
     private fun getDotAt(d1: Dot?, d2: Dot?, pos: Float) : Dot{
@@ -339,6 +342,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun drawSpline(dotsList: MutableList<MainDot>, mutableBitmap: Bitmap) {
+
+        val width = mutableBitmap.width
+        val height = mutableBitmap.height
 
         if (dotsList.size > 1) {
 
@@ -373,11 +379,35 @@ class MainActivity : AppCompatActivity() {
                     drawLine(mutableBitmap, prevDot, dot, 2)
                     prevDot = dot
                 }
+
+                if (FXAA_ON) {
+
+                    prevDot = dotsList[i]
+
+                    val destPixels = IntArray(width * height)
+                    mutableBitmap.getPixels(destPixels, 0, width, 0, 0, width, height)
+
+                    for (time in 0..iterations) {
+
+                        val t = time.toFloat() / iterations
+
+                        val sqrDot0 = getDotAt(dot0, dot1, t)
+                        val sqrDot1 = getDotAt(dot1, dot2, t)
+                        val sqrDot2 = getDotAt(dot2, dot3, t)
+
+                        val cubDot0 = getDotAt(sqrDot0, sqrDot1, t)
+                        val cubDot1 = getDotAt(sqrDot1, sqrDot2, t)
+
+                        val dot = getDotAt(cubDot0, cubDot1, t)
+
+                        lineFXAA(mutableBitmap, destPixels, prevDot, dot, 2)
+                        mutableBitmap.setPixels(destPixels, 0, width, 0, 0, width, height)
+                        prevDot = dot
+                    }
+                }
             }
 
         }
-
-        binding.imageView.setImageBitmap(mutableBitmap)
     }
 
     private fun drawLine(mutableBitmap: Bitmap, dot1: Dot, dot2: Dot, strokeSize: Int) {
@@ -420,6 +450,148 @@ class MainActivity : AppCompatActivity() {
                 y += sy
             }
         }
+    }
+
+    private fun lineFXAA(mutableBitmap: Bitmap, destPixels: IntArray, dot1: Dot, dot2: Dot, strokeSize: Int) {
+
+        val fxaaStroke = strokeSize + 1
+
+        val width = mutableBitmap.width
+        val height = mutableBitmap.height
+
+        val x1 = dot1.getX().toInt()
+        val y1 = dot1.getY().toInt()
+        val x2 = dot2.getX().toInt()
+        val y2 = dot2.getY().toInt()
+
+        val dx = Math.abs(x2 - x1)
+        val dy = Math.abs(y2 - y1)
+
+        val sx = if (x1 < x2) 1 else -1
+        val sy = if (y1 < y2) 1 else -1
+
+        var err = dx - dy
+        var e2: Int
+
+        var x = x1
+        var y = y1
+
+        while (true) {
+            for (i in -fxaaStroke..fxaaStroke) {
+                for (j in -fxaaStroke..fxaaStroke) {
+                    if (x + i in 0 until mutableBitmap.width && y + j in 0 until mutableBitmap.height) {
+                        FXAA(mutableBitmap, destPixels, x + i, y + j)
+                    }
+                }
+            }
+
+            if (x == x2 && y == y2) break
+
+            e2 = 2 * err
+            if (e2 > -dy) {
+                err -= dy
+                x += sx
+            }
+            if (e2 < dx) {
+                err += dx
+                y += sy
+            }
+        }
+    }
+
+    private fun FXAA(mutableBitmap: Bitmap, destPixels: IntArray, x: Int, y: Int) {
+
+        val width = mutableBitmap.width
+        val height = mutableBitmap.height
+
+        val spanMax = 8.0
+        val reduceMin = 1.0 / 128.0
+        val reduceMul = 1.0 / 8.0
+
+        val grayTL = getGrayscaleValue(mutableBitmap.getPixel(
+            Math.min(Math.max(x - 1, 0), width - 1),
+            Math.min(Math.max(y - 1, 0), height - 1)
+        ))
+
+        val grayTR = getGrayscaleValue(mutableBitmap.getPixel(
+            Math.min(Math.max(x + 1, 0), width - 1),
+            Math.min(Math.max(y - 1, 0), height - 1)
+        ))
+
+        val grayBL = getGrayscaleValue(mutableBitmap.getPixel(
+            Math.min(Math.max(x - 1, 0), width - 1),
+            Math.min(Math.max(y + 1, 0), height - 1)
+        ))
+
+        val grayBR = getGrayscaleValue(mutableBitmap.getPixel(
+            Math.min(Math.max(x + 1, 0), width - 1),
+            Math.min(Math.max(y + 1, 0), height - 1)
+        ))
+
+        val grayM = getGrayscaleValue(mutableBitmap.getPixel(
+            Math.min(Math.max(x, 0), width - 1),
+            Math.min(Math.max(y, 0), height - 1)
+        ))
+
+        val initDirX = -((grayTL + grayTR) - (grayBL + grayBR))
+        val initDirY = ((grayTL + grayBL) - (grayTR + grayBR))
+
+        val dirReduce = Math.max((grayTL + grayTR + grayBL + grayBR) * reduceMul * 0.25, reduceMin)
+        val inverseDirAdjust = 1.0 / (Math.min(abs(initDirX), abs(initDirY)) + dirReduce)
+
+        val dirX = Math.min(Math.max(initDirX * inverseDirAdjust, -spanMax), spanMax)
+        val dirY = Math.min(Math.max(initDirY * inverseDirAdjust, -spanMax), spanMax)
+
+        val pixel11 = mutableBitmap.getPixel(
+            Math.min(Math.max(x + (dirX * (1.0 / 3.0 - 0.5)).toInt(), 0), width - 1),
+            Math.min(Math.max(y + (dirY * (1.0 / 3.0 - 0.5)).toInt(), 0), height - 1)
+        )
+
+        val pixel12 = mutableBitmap.getPixel(
+            Math.min(Math.max(x + (dirX * (2.0 / 3.0 - 0.5)).toInt(), 0), width - 1),
+            Math.min(Math.max(y + (dirY * (2.0 / 3.0 - 0.5)).toInt(), 0), height - 1)
+        )
+
+        val pixel21 = mutableBitmap.getPixel(
+            Math.min(Math.max(x + (dirX * -0.5).toInt(), 0), width - 1),
+            Math.min(Math.max(y + (dirY * -0.5).toInt(), 0), height - 1)
+        )
+
+        val pixel22 = mutableBitmap.getPixel(
+            Math.min(Math.max(x + (dirX * 0.5).toInt(), 0), width - 1),
+            Math.min(Math.max(y + (dirY * 0.5).toInt(), 0), height - 1)
+        )
+
+        val result1 = Color.argb(
+            255,
+            (0.5 * (Color.red(pixel11) + Color.red(pixel12))).toInt(),
+            (0.5 * (Color.green(pixel11) + Color.green(pixel12))).toInt(),
+            (0.5 * (Color.blue(pixel11) + Color.blue(pixel12))).toInt()
+        )
+
+        val result2 = Color.argb(
+            255,
+            (Color.red(result1) * 0.5 + 0.25 * (Color.red(pixel21) + Color.red(pixel22))).toInt(),
+            (Color.green(result1) * 0.5 + 0.25 * (Color.green(pixel21) + Color.green(pixel22))).toInt(),
+            (Color.blue(result1) * 0.5 + 0.25 * (Color.blue(pixel21) + Color.blue(pixel22))).toInt()
+        )
+
+        val grayMin = Math.min(Math.min(Math.min(grayM, grayBR), Math.min(grayBL, grayTR)), grayTL)
+        val grayMax = Math.max(Math.max(Math.max(grayM, grayBR), Math.max(grayBL, grayTR)), grayTL)
+
+        val grayRes2 = getGrayscaleValue(result2)
+
+        if (grayRes2 < grayMin || grayRes2 > grayMax) {
+                    destPixels[y * width + x] = result1
+        }
+
+        else {
+            destPixels[y * width + x] = result2
+        }
+    }
+
+    private fun getGrayscaleValue(pixel: Int): Int {
+        return (0.299 * Color.red(pixel) + 0.587 * Color.green(pixel) + 0.114 * Color.blue(pixel)).toInt()
     }
 }
 
